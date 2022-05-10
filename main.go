d25887ae5bba3074bdd76269e7b00c005b201c45
Yahya <chahineyahya@pm.me>
2022-05-10 14:58:21 +0100

    Added the count() function

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	fmt.Println(count(os.Stdin))
+}
+
+func count(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+	wordCount := 0
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		wordCount++
+	}
+
+	return wordCount
+}
