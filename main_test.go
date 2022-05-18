e51676318e236e192350175063239e03249ea4ee
Yahya <chahineyahya@pm.me>
2022-05-18 12:05:13 +0100

    Added test case for the Line Count functionality

--- main_test.go
+++ main_test.go
@@ -8,7 +8,17 @@ import (
 func TestWordCount(t *testing.T) {
 	b := bytes.NewBufferString("testing the word count function\n")
 	expected := 5
-	got := count(b)
+	got := count(b, false)
+
+	if expected != got {
+		t.Errorf("Expected %d, got %d instead", expected, got)
+	}
+}
+
+func TestLineCount(t *testing.T) {
+	b := bytes.NewBufferString("Line1 Word2 Word3\nLine2\nLine3\nLine4")
+	expected := 4
+	got := count(b, true)
 
 	if expected != got {
 		t.Errorf("Expected %d, got %d instead", expected, got)
